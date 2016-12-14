0c820893cc911a70489fc81465322338b13e0c8b
michel van dijck <michvd92@gmail.com>
2016-12-14 11:31:28 +0100

    profilepage WIP

---
+++ source/javascripts/scroll.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+
+  $("#abt").click(function() {
+   $('html,body').animate({
+     scrollTop: $(".about-page").offset().top},
+     'slow');
+ });
+
+  $("#wrk").click(function() {
+   $('html,body').animate({
+     scrollTop: $(".experience-page").offset().top},
+     'slow');
+ });
+
+  $("#cntct").click(function() {
+   $('html,body').animate({
+     scrollTop: $(".contact-page").offset().top},
+     'slow');
+ });
+
+});
+
